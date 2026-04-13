cb2510375f7cde7f3d348aa1e1d88f1e4bcbed52
Orekiyuta <orekiyuta@gmail.com>
2026-04-13 09:57:11 +0800

    feat: add output call local python

--- terraform/modules/use_cases/call_local_python/main.tf
+++ terraform/modules/use_cases/call_local_python/main.tf
@@ -32,3 +32,42 @@ output "python_resp" {
   value = data.external.call_python.result
 }
 
+# (.venv) terraform % terraform plan -target "module.use_cases_call_local_python"
+# module.use_cases_call_local_python.data.external.call_python: Reading...
+# module.use_cases_call_local_python.data.external.call_python: Read complete after 2s [id=-]
+#
+# Changes to Outputs:
+#   + use_case_call_local_python = {
+#       + python_resp = {
+#           + result = jsonencode(
+#                 {
+#                   + args    = {}
+#                   + data    = jsonencode(
+#                         {
+#                           + env  = "dev"
+#                           + name = "terraform"
+#                         }
+#                     )
+#                   + files   = {}
+#                   + form    = {}
+#                   + headers = {
+#                       + Accept          = "*/*"
+#                       + Accept-Encoding = "gzip, deflate, zstd"
+#                       + Content-Length  = "35"
+#                       + Content-Type    = "application/json"
+#                       + Host            = "httpbin.org"
+#                       + User-Agent      = "python-requests/2.33.1"
+#                       + X-Amzn-Trace-Id = "Root=1-69dc4d07-18463cf747cba4374205a52e"
+#                     }
+#                   + json    = {
+#                       + env  = "dev"
+#                       + name = "terraform"
+#                     }
+#                   + origin  = "45.64.247.97"
+#                   + url     = "https://httpbin.org/post"
+#                 }
+#             )
+#         }
+#     }
+#
+# You can apply this plan to save these new output values to the Terraform state, without changing any real infrastructure.
